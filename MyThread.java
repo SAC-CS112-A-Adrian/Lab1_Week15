c39ea46b588dd66035169486335f2408abcf7536
SAC-CS112-A-Adrian <pw2422@gmail.com>
2016-05-25 20:38:34 -0700

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -1,15 +1,21 @@
 
 public class MyThread implements Runnable {
 
-	private final int diceRolled;
+	private final int counter;
 
-	public MyThread(int diceRolled) {
-		this.diceRolled = diceRolled;
+	public MyThread(int counter) {
+		this.counter = counter;
 	}
 
 	@Override
 	public void run() {
-		int num = (1 + (int) (Math.random() * 6));
+		
+		int num = 0;
+		for (long i = 1; i < counter; i++) {
+		      num = (1 + (int) (Math.random() * 6));
+		    }
+
+		
 		System.out.printf("%d\t", num);
 	}
 
